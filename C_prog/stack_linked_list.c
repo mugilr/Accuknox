22c72dc4f1abf4e2bfbdc2b69528c6426625f910
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-05-03 17:49:05 +0530

    stack using linked list

---
+++ C_prog/stack_linked_list.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct node {
+    int data;
+    struct node* next;
+};
+ 
+struct node* new(int data)
+{
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+ 
+int isEmpty(struct node* root)
+{
+    return !root;
+}
+ 
+void push(struct node** root, int data)
+{
+    struct node* node = new(data);
+    node->next = *root;
+    *root = node;
+    printf("%d pushed\n", data);
+}
+ 
+int pop(struct node** root)
+{
+    if (isEmpty(*root))
+        return -1;
+    struct node* temp = *root;
+    *root = (*root)->next;
+    int popped = temp->data;
+    free(temp);
+ 
+    return popped;
+}
+ 
+int top(struct node* root)
+{
+    if (isEmpty(root))
+        return -1;
+    return root->data;
+}
+ 
+int main()
+{
+    struct node* root = NULL;
+ 
+    push(&root, 1);
+    push(&root, 2);
+    push(&root, 3);
+ 
+    printf("%d popped\n", pop(&root));
+    printf("%d popped\n", pop(&root)); 
+    printf("Top element: %d\n", top(root));
+ 
+    return 0;
+}
