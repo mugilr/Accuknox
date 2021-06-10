744dc30fdf90e44eae2d4de1781451dbb9ab5370
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-06-10 14:15:25 +0530

    ebpf:tcp_block: Add trace log for block
    the below diff will show the changes in the ebpf file and that can be
    changed accordingly
    
    Signed-off-by: Mugilraj Dhavachelvan <dmugil2000@gmail.com>

--- ebpf/tcp_block.c
+++ ebpf/tcp_block.c
@@ -6,6 +6,12 @@
 #include <linux/in.h>
 #include <linux/tcp.h>
 
+#ifndef SEC
+#define SEC(NAME) __attribute__((section(NAME), used))
+#endif
+
+
+SEC("drop")
 int filter(struct xdp_md *ctx) {
   bpf_trace_printk("got a packet\n");
   void *data = (void *)(long)ctx->data;
@@ -17,6 +23,7 @@ int filter(struct xdp_md *ctx) {
       if (ip->protocol == IPPROTO_TCP) {
         struct tcphdr *tcp = (void*)ip + sizeof(*ip);
         if ((void*)tcp + sizeof(*tcp) <= data_end) {
+          bpf_trace_printk("tcp port: %d\n", ntohs(tcp->source));
           if (tcp->source == ntohs(8080)) {
             bpf_trace_printk("tcp port 8080 packet dropped\n");
             return XDP_DROP;
