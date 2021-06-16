8e2225a62175a655eea50e6949689abc3002955a
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-06-16 15:50:56 +0530

    just

--- ebpf/runtime_config.py
+++ ebpf/runtime_config.py
@@ -4,9 +4,23 @@ prog = """
 #define KBUILD_MODNAME "filter"
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
+
 #include <linux/ip.h>
+
 #include <linux/in.h>
 #include <linux/tcp.h>
+
+
+/*
+static struct tcphdr *tcp_(struct xdp_md *ctx){
+  bpf_trace_printk("got a packet\\n");
+  void *data = (void *)(long)ctx->data;
+  void *data_end = (void *)(long)ctx->data_end;
+  struct ethhdr *eth = data;
+  if ((void*)eth + sizeof(*eth) <= data_end) {
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void*)ip + sizeof(*ip) <= data_end) {
+ 
 
 /*
 static struct tcphdr *tcp_(struct xdp_md *ctx){
@@ -25,12 +39,13 @@ static struct tcphdr *tcp_(struct xdp_md *ctx){
    }
    return 0;
 }
-*/
 
-int xdp_drop(struct xdp_md *ctx) {
+int xdp_drop(struct xdp_md *ctx) 
+{
   bpf_trace_printk("got a packet\\n");
   void *data = (void *)(long)ctx->data;
   void *data_end = (void *)(long)ctx->data_end;
+
 
   struct ethhdr *eth = data;
   if ((void*)eth + sizeof(*eth) <= data_end) {
@@ -117,8 +132,8 @@ fn3 = b.load_func("xdp_abort", BPF.XDP)
 #fn = b.load_func("filter", BPF.XDP)
 #b.attach_xdp(device, fn, 0)
 
-try:
-  while True:
+try
+  uhile True:
    todo=input("Enter th config:\n1 - DROP \n2 - PASS \n3 - ABORT\n")
    if (todo == '1'):
      b.attach_xdp(device, fn1, 0)
@@ -132,3 +147,5 @@ except KeyboardInterrupt:
   pass
 
 b.remove_xdp(device, 0)
+
+
