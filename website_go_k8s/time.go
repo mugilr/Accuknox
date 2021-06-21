7c89d033677fd4e1edc0185253178a0b550e794d
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-06-22 01:56:43 +0530

    update dockerfile

--- website_go_k8s/time.go
+++ website_go_k8s/time.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-	//fmt.Println("The current datetime is:", now)
+	fmt.Println("The current datetime is:", now)
 	fmt.Fprintf(w, "Welcome to GREAT KIRIKALAN MAGIC SHOW\n\n")
 	ti := now.Format(time.Stamp)
 	fmt.Fprintf(w, "Date & Time : %s \n\n", ti)
