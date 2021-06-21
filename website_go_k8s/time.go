9313d349753ad349a7851008284f49b89f5f4a72
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-06-21 21:41:55 +0530

    add k8s yaml files

--- website_go_k8s/time.go
+++ website_go_k8s/time.go
@@ -10,10 +10,9 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
 	//fmt.Println("The current datetime is:", now)
-	fmt.Fprintf(w, "Welcome to GREAT KARIKALAN MAGIC SHOW\n\n")
+	fmt.Fprintf(w, "Welcome to GREAT KIRIKALAN MAGIC SHOW\n\n")
 	ti := now.Format(time.Stamp)
-	fmt.Fprintf(w, "Date & Time : %s \n", ti)
-
+	fmt.Fprintf(w, "Date & Time : %s \n\n", ti)
 }
 
 func main() {
